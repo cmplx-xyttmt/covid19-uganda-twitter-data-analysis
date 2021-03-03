eb9f57aa6035d9e1f6c0bbe1fe01bbcb18ece2dc
Isaac Owomugisha <archimedesisaac@gmail.com>
2021-03-03 17:55:06 +0300

    Group tweets into groups of 20000 to prevent out of memory error

--- uganda_tweets_json.py
+++ uganda_tweets_json.py
@@ -2,27 +2,16 @@ import io
 import json
 
 from data_collection.twitterv2.db_utils import retrieve_tweets
+from ReadTweetsFromJson import grouper
 
 
 if __name__ == '__main__':
-    tweets = retrieve_tweets("ugandan")
-    # Divide into 2 files
-    first = len(tweets)//2
-    with io.open('data/ugandan_tweets_1.json', 'w', encoding='utf-8') as f:
-        tweets_no_id = []
-        for tweet in tweets:
-            tweet.pop('_id')
-            tweets_no_id.append(tweet)
-            if len(tweets_no_id) == first:
-                break
-        f.write(json.dumps({"tweets": tweets_no_id}, ensure_ascii=False, indent=5))
-
-    with io.open('data/ugandan_tweets_2.json', 'w', encoding='utf-8') as f:
-        tweets_no_id = []
-        for i in range(first, len(tweets)):
-            tweet = tweets[i]
-            tweet.pop('_id')
-            tweets_no_id.append(tweet)
-            if len(tweets_no_id) == first:
-                break
-        f.write(json.dumps({"tweets": tweets_no_id}, ensure_ascii=False, indent=5))
+    tweets_id = retrieve_tweets("ugandan")
+    tweets = []
+    for tweet in tweets_id:
+        tweet.pop('_id')
+        tweets.append(tweet)
+    for i, group in enumerate(grouper(tweets, 20000)):
+        filename = f'data/ug_tweets_{i + 1}.json'
+        with io.open(filename, 'w', encoding='utf-8') as f:
+            f.write(json.dumps({"tweets": list(group)}, ensure_ascii=False, indent=5))
