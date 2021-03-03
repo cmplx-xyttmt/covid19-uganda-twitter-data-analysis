c019335a33745c6c34a34b87917a6897612fa302
Isaac Owomugisha <archimedesisaac@gmail.com>
2021-03-03 17:28:49 +0300

    Separate tweets into 2 json files

--- uganda_tweets_json.py
+++ uganda_tweets_json.py
@@ -6,9 +6,23 @@ from data_collection.twitterv2.db_utils import retrieve_tweets
 
 if __name__ == '__main__':
     tweets = retrieve_tweets("ugandan")
-    with io.open('data/ugandan_tweets.json', 'w', encoding='utf-8') as f:
+    # Divide into 2 files
+    first = len(tweets)//2
+    with io.open('data/ugandan_tweets_1.json', 'w', encoding='utf-8') as f:
         tweets_no_id = []
         for tweet in tweets:
             tweet.pop('_id')
             tweets_no_id.append(tweet)
+            if len(tweets_no_id) == first:
+                break
+        f.write(json.dumps({"tweets": tweets_no_id}, ensure_ascii=False, indent=5))
+
+    with io.open('data/ugandan_tweets_2.json', 'w', encoding='utf-8') as f:
+        tweets_no_id = []
+        for i in range(first, len(tweets)):
+            tweet = tweets[i]
+            tweet.pop('_id')
+            tweets_no_id.append(tweet)
+            if len(tweets_no_id) == first:
+                break
         f.write(json.dumps({"tweets": tweets_no_id}, ensure_ascii=False, indent=5))
