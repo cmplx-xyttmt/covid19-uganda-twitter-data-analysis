304d2acf69318b3094e0d2cd2b9f3f9e062358b8
Isaac Owomugisha <archimedesisaac@gmail.com>
2021-03-03 18:13:12 +0300

    Add deletion of files after they're transferred to prevent persistent memory error

--- uganda_tweets_json.py
+++ uganda_tweets_json.py
@@ -1,8 +1,24 @@
 import io
 import json
+import os
+import shutil
+import time
 
-from data_collection.twitterv2.db_utils import retrieve_tweets
 from ReadTweetsFromJson import grouper
+from data_collection.twitterv2.db_utils import retrieve_tweets
+
+
+def delete_files():
+    folder = 'data'
+    for file_name in os.listdir(folder):
+        file_path = os.path.join(folder, file_name)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 
 if __name__ == '__main__':
@@ -15,3 +31,7 @@ if __name__ == '__main__':
         filename = f'data/ug_tweets_{i + 1}.json'
         with io.open(filename, 'w', encoding='utf-8') as f:
             f.write(json.dumps({"tweets": list(group)}, ensure_ascii=False, indent=5))
+        if (i + 1) % 10 == 0:
+            print("Waiting for 2 minutes, transfer files quickly before they are deleted")
+            time.sleep(120)
+            delete_files()
